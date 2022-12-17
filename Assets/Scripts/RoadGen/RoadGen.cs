90a23c7c8ad59a3c623f4cb03a9c0aed3eec3ffe
WinsentXorandif <xorandif@gmail.com>
2022-12-17 04:45:19 +0300

    navigation
    navigation?

---
+++ Assets/Scripts/RoadGen/RoadGen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGen : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject groung;
+    [SerializeField]
+    private GameObject hero;
+
+}
